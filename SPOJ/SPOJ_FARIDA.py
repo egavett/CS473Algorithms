e088c53367e9a8ddc23fc24f961bac5289b8cfd6
Elijah Gavett <egavett35@gmail.com>
2017-10-31 11:35:40 -0700

    Finished farida, created party

--- SPOJ/SPOJ_FARIDA.py
+++ SPOJ/SPOJ_FARIDA.py
@@ -1,24 +1,14 @@
-
 def maxCoins(array):
-    maxArray = []
-    maxArray.append(0)
-    maxArray.append(array[0])
-    
-    for index in range(2, len(array)):
-        print "{}, {}".format(index-1, index-2)
-        print maxArray
-        print "Comapring {}+{} and {}".format(array[index], maxArray[index-2], maxArray[index-1])
-        newMax = max(array[index] + maxArray[index-2], maxArray[index-1])
-        print "Chose {}".format(newMax)
+    maxArray = [0, array[0]]
+
+    for index in range(2, len(array)+1):
+        newMax = max(array[index-1] + maxArray[index-2], maxArray[index-1])
         maxArray.append(newMax)
     return maxArray[-1]
 
-
-
-
 numberOfCases = int(input())
-for _ in range(numberOfCases):
-    ingoreThis = int(input())
-    inputStrings = str(raw_input()).strip().split()
+for i in range(numberOfCases):
+    ingoreThis = int(input()) #parsing works without needed to know the length of the array
+    inputStrings = raw_input().strip().split()
     inputArray = [int(x) for x in inputStrings]
-    print maxCoins(inputArray)
+    print("Case %d: %d" % (i+1, maxCoins(inputArray)))
